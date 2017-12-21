69260adf115296d943b1004ed4ca4e0e95f2f3e1
amishiro <amishiro+github@gmail.com>
2017-12-22 06:09:11 +0900

    ショルダーナビ完成 #1

--- public/_layout/index.php
+++ public/_layout/index.php
@@ -6,24 +6,21 @@ $metaDesc = $siteDescription;
 $body_class ="l-page-single";
 include($inc_path."/lib/inc/head.php");
 include($inc_path."/lib/inc/header.php");
+include($inc_path."/lib/inc/shoulder-nav.php");
 include($inc_path."/lib/inc/main-head.php");
-include($inc_path."/lib/inc/pan.php");
 ?>
 
+
+
+
+
+<?php #include($inc_path."/lib/inc/pan.php"); ?>
 <div class="main__conts conts">
   <section class="conts__item">
     <h2 class="u-ttl_l">ページレイアウト</h2>
   </section>
   <section class="conts__item">
-    <h2 class="u-ttl_l">レイアウト用パーツ</h2>
-    <h4 class="u-ttl_s">
-      <a href="<?php echo $base_url; ?>/_styleguide/category/common/layout/l-full/" target="_blank" class="u-icon__link">.l-full</a>
-    </h4>
-    <p>親要素から子要素をはみ出して表示</p>
-    <h4 class="u-ttl_s">
-      <a href="<?php echo $base_url; ?>/_styleguide/category/common/layout/l-column/" target="_blank" class="u-icon__link">.l-culumn</a>
-    </h4>
-    <p>要素の隙間ありのカラムレイアウト</p>
+    <?php include("./sample-article.php"); ?>
   </section>
 </div>
 
---
+++ public/lib/inc/shoulder-nav.php
@@ -0,0 +1,26 @@
+<nav class="shoulder-nav">
+  <div class="shoulder-nav__bg"></div>
+  <div class="shoulder-nav__row">
+    <p class="shoulder-nav__label">LANGUAGE</p>
+    <nav class="shoulder-nav__lang snav-lang">
+      <ul class="snav-lang__row">
+        <li class="snav-lang__item">
+          <a href="" class="snav-lang__link jp">JP</a>
+        </li>
+        <li class="snav-lang__item">
+          <a href="" class="snav-lang__link en">EN</a>
+        </li>
+      </ul>
+    </nav>
+    <label class="shoulder-nav__menu smenu">
+      <div class="smenu__row">
+        <div class="smenu__label">MENU</div>
+        <div class="smenu__bar smenu-bar">
+          <span class="smenu-bar__item"></span>
+          <span class="smenu-bar__item"></span>
+          <span class="smenu-bar__item"></span>
+        </div>
+      </div>
+    </label>
+  </div>
+</nav>
