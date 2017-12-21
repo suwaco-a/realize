9e49be652565dedee26d908ec2825defc81146b6
amishiro <amishiro+github@gmail.com>
2017-12-21 18:04:01 +0900

    footer完了 #1

--- public/lib/inc/footer.php
+++ public/lib/inc/footer.php
@@ -1,17 +1,27 @@
 <footer class="footer">
   <div class="footer__row">
 
-
-    Realize Mobile Communications Corp
-
-    サイトポリシー
-    情報セキュリティポリシー
-    個人情報について
+    <nav class="footer__flink flink">
+      <ul class="flink__row">
+        <li class="flink__item logo">
+          <a href="<?php echo $base_url; ?>" class="flink__link logo">Realize Mobile Communications Corp</a>
+        </li>
+        <li class="flink__item">
+          <a href="<?php echo $base_url; ?>" class="flink__link">サイトポリシー</a>
+        </li>
+        <li class="flink__item">
+          <a href="<?php echo $base_url; ?>" class="flink__link">情報セキュリティポリシー</a>
+        </li>
+        <li class="flink__item">
+          <a href="<?php echo $base_url; ?>" class="flink__link">個人情報について</a>
+        </li>
+      </ul>
+    </nav>
 
     <nav class="footer__nav fnav">
       <ul class="fnav__row">
         <li class="fnav__item service fnav-deep">
-          <a href="<?php echo $base_url; ?>/service/">SERVICE<span class="fnav__jp">事業内容</span></a>
+          <a href="<?php echo $base_url; ?>/service/" class="fnav__link">SERVICE<span class="fnav__jp">事業内容</span></a>
 
           <nav class="fnav-deep__nav">
             <ul class="fnav-deep__row">
@@ -26,7 +36,7 @@
 
         </li>
         <li class="fnav__item company fnav-deep">
-          <a href="<?php echo $base_url; ?>/company/">COMPANY<span class="fnav__jp">会社概要</span></a>
+          <a href="<?php echo $base_url; ?>/company/" class="fnav__link">COMPANY<span class="fnav__jp">会社概要</span></a>
 
           <nav class="fnav-deep__nav">
             <ul class="fnav-deep__row">
@@ -53,7 +63,7 @@
 
         </li>
         <li class="fnav__item recruit fnav-deep">
-          <a href="<?php echo $base_url; ?>/recruit/">RECRUIT<span class="fnav__jp">採用情報</span></a>
+          <a href="<?php echo $base_url; ?>/recruit/" class="fnav__link">RECRUIT<span class="fnav__jp">採用情報</span></a>
 
           <nav class="fnav-deep__nav">
             <ul class="fnav-deep__row">
@@ -68,7 +78,7 @@
 
         </li>
         <li class="fnav__item news fnav-deep">
-          <a href="<?php echo $base_url; ?>/news/">NEWS<span class="fnav__jp">ニュース</span></a>
+          <a href="<?php echo $base_url; ?>/news/" class="fnav__link">NEWS<span class="fnav__jp">ニュース</span></a>
 
           <nav class="fnav-deep__nav">
             <ul class="fnav-deep__row">
@@ -83,7 +93,7 @@
 
         </li>
         <li class="fnav__item contact fnav-deep">
-          <a href="<?php echo $base_url; ?>/contact/">CONTACT<span class="fnav__jp">お問い合わせ</span></a>
+          <a href="<?php echo $base_url; ?>/contact/" class="fnav__link">CONTACT<span class="fnav__jp">お問い合わせ</span></a>
 
           <nav class="fnav-deep__nav">
             <ul class="fnav-deep__row">
@@ -96,8 +106,8 @@
         </li>
       </ul>
     </nav>
-    <address class="footer__add footadr">
-      <div class="footadr__copyright">Copyright<span class="footadr__mark">&copy;</span> Realize Mobile Communications Corp. All rights reserved.</div>
-    </address>
   </div>
+  <address class="footer__add footadr">
+    <div class="footadr__copyright">Copyright&nbsp;<span class="footadr__mark">&copy;</span> Realize Mobile Communications Corp. All rights reserved.</div>
+  </address>
 </footer>
