1e3f46f219ad4d4ad4efdd46d744ada6e4cc3289
amishiro <amishiro+github@gmail.com>
2017-12-21 15:50:11 +0900

    head＆サムネ関連マークアップ #1

--- public/lib/functions/htmlMetas.php
+++ public/lib/functions/htmlMetas.php
@@ -2,8 +2,8 @@
 
 #メタデーター管理
 
-$siteName = "AmiTemplate";
-$siteDescription = $siteName."の".$pageTtl."ページです。サイトディスクリプション";
+$siteName = "Realize Mobile Communications Corp.";
+$siteDescription = $siteName."の".$pageTtl."ページです。「Realizing  people's dreams」先端技術を付加価値に変え、イノベイティブな事業開発を支援。";
 
 global $pageTtl;
 
