ce17f7cb65ee9655522378a368964bbafb0677ea
amishiro <amishiro+github@gmail.com>
2017-12-21 16:47:32 +0900

    footerマークアップ #1

--- public/lib/inc/footer.php
+++ public/lib/inc/footer.php
@@ -1,30 +1,103 @@
 <footer class="footer">
   <div class="footer__row">
 
+
+    Realize Mobile Communications Corp
+
+    サイトポリシー
+    情報セキュリティポリシー
+    個人情報について
+
     <nav class="footer__nav fnav">
       <ul class="fnav__row">
-        <li class="fnav__item_home">
-          <a href="<?php echo $base_url; ?>/">home</a>
+        <li class="fnav__item service fnav-deep">
+          <a href="<?php echo $base_url; ?>/service/">SERVICE<span class="fnav__jp">事業内容</span></a>
+
+          <nav class="fnav-deep__nav">
+            <ul class="fnav-deep__row">
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">ソリューション</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">コンテンツ</a>
+              </li>
+            </ul>
+          </nav>
+
         </li>
-        <li class="fnav__item_sample">
-          <a href="<?php echo $base_url; ?>/_sample/">sample</a>
+        <li class="fnav__item company fnav-deep">
+          <a href="<?php echo $base_url; ?>/company/">COMPANY<span class="fnav__jp">会社概要</span></a>
+
+          <nav class="fnav-deep__nav">
+            <ul class="fnav-deep__row">
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">会社概要</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">企業理念</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">社長挨拶</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">役員一覧</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">沿革</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">アクセス</a>
+              </li>
+            </ul>
+          </nav>
+
         </li>
-        <li class="fnav__item_layout">
-          <a href="<?php echo $base_url; ?>/_layout/">layout</a>
+        <li class="fnav__item recruit fnav-deep">
+          <a href="<?php echo $base_url; ?>/recruit/">RECRUIT<span class="fnav__jp">採用情報</span></a>
+
+          <nav class="fnav-deep__nav">
+            <ul class="fnav-deep__row">
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">新卒採用</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">キャリア採用</a>
+              </li>
+            </ul>
+          </nav>
+
         </li>
-        <li class="fnav__item">
-          <a href="#">menu</a>
+        <li class="fnav__item news fnav-deep">
+          <a href="<?php echo $base_url; ?>/news/">NEWS<span class="fnav__jp">ニュース</span></a>
+
+          <nav class="fnav-deep__nav">
+            <ul class="fnav-deep__row">
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">プレスリリース</a>
+              </li>
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>" class="fnav-deep__link">お知らせ</a>
+              </li>
+            </ul>
+          </nav>
+
         </li>
-        <li class="fnav__item">
-          <a href="#">menu</a>
-        </li>
-        <li class="fnav__item_contact">
-          <a href="#">contact</a>
+        <li class="fnav__item contact fnav-deep">
+          <a href="<?php echo $base_url; ?>/contact/">CONTACT<span class="fnav__jp">お問い合わせ</span></a>
+
+          <nav class="fnav-deep__nav">
+            <ul class="fnav-deep__row">
+              <li class="fnav-deep__item">
+                <a href="<?php echo $base_url; ?>/contact" class="fnav-deep__link">お問い合わせ</a>
+              </li>
+            </ul>
+          </nav>
+
         </li>
       </ul>
     </nav>
     <address class="footer__add footadr">
-      <div class="footadr__copyright"><span class="footadr__mark">&copy;</span> amiten Inc. All Rights Reserved.</div>
+      <div class="footadr__copyright">Copyright<span class="footadr__mark">&copy;</span> Realize Mobile Communications Corp. All rights reserved.</div>
     </address>
   </div>
 </footer>
