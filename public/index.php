8bc5c6e761f87b8078f0e27e8f91926bb57f8b48
amishiro <amishiro+github@gmail.com>
2017-12-24 08:54:33 +0900

    スクロールコントロール #3

--- public/index.php
+++ public/index.php
@@ -10,14 +10,13 @@ include($inc_path."/lib/inc/shoulder-nav.php");
 include($inc_path."/lib/inc/main-head.php");
 ?>
 
-<div class="home-conts">
+<div class="main__conts home-conts">
 
   <section class="home-conts__item" id="realizing">
 
     <div class="home-conts__inner realizing">
       <div class="home-conts__bg">
-        <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video">
-          <source src="<?php echo $img_url; ?>/home/realizing.mp4" type="video/mp4">
+        <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video"><source src="<?php echo $img_url; ?>/home/realizing.mp4" type="video/mp4">
         </video>
         <div class="home-conts__dot"></div>
       </div>
@@ -37,141 +36,135 @@ include($inc_path."/lib/inc/main-head.php");
     </div>
   </section>
 
-    <section class="home-conts__item" id="mixed">
-      <div class="home-conts__inner mixed">
+  <section class="home-conts__item" id="mixed">
+    <div class="home-conts__inner mixed">
 
-        <div class="home-conts__bg">
-          <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video">
-            <source src="<?php echo $img_url; ?>/home/mixed.mp4" type="video/mp4">
-          </video>
-          <div class="home-conts__dot"></div>
-        </div>
-        <header class="home-conts__head">
-          <h2 class="home-conts__ttl">MIXED REALITYS</h2>
-          <p class="home-conts__sub">VR（仮想現実）AR（拡張現実）MR（複合現実）のスペシャリスト</p>
-        </header>
-        <p class="home-conts__ex">3D/ホログラム技術によりこれまでには実現できなかったイノベーションを</p>
-        <div class="home-conts__next next">
-          <a href="#mobility" class="next__link" data-scroll>
-            <div class="next__arrow">NEXT</div>
-            <h4 class="next__ttl">SMART MOBILITY<br>MANAGEMENT</h4>
-            <p class="next__sub">社会を前進させる技術</p>
-          </a>
-          <div class="next__bg"></div>
-        </div>
+      <div class="home-conts__bg">
+        <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video"><source src="<?php echo $img_url; ?>/home/mixed.mp4" type="video/mp4">
+        </video>
+        <div class="home-conts__dot"></div>
       </div>
-    </section>
-
-    <section class="home-conts__item" id="mobility">
-      <div class="home-conts__inner mobility">
-        <div class="home-conts__bg">
-          <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video">
-            <source src="<?php echo $img_url; ?>/home/mobility.mp4" type="video/mp4">
-          </video>
-          <div class="home-conts__dot"></div>
-        </div>
-        <header class="home-conts__head">
-          <h2 class="home-conts__ttl">SMART MOBILITY<br>MANAGEMENT</h2>
-          <p class="home-conts__sub">社会を前進させる技術</p>
-        </header>
-        <p class="home-conts__ex">シェアリングエコノミーから物流課題までICTでモビリティの未来を創る</p>
-        <div class="home-conts__next next">
-          <a href="#mobility" class="next__link" data-scroll>
-            <div class="next__arrow">NEXT</div>
-            <h4 class="next__ttl">SENSING/AI</h4>
-            <p class="next__sub">データの意味を見出し新たな価値を創る</p>
-          </a>
-          <div class="next__bg"></div>
-        </div>
+      <header class="home-conts__head">
+        <h2 class="home-conts__ttl">MIXED REALITYS</h2>
+        <p class="home-conts__sub">VR（仮想現実）AR（拡張現実）MR（複合現実）のスペシャリスト</p>
+      </header>
+      <p class="home-conts__ex">3D/ホログラム技術によりこれまでには実現できなかったイノベーションを</p>
+      <div class="home-conts__next next">
+        <a href="#mobility" class="next__link" data-scroll>
+          <div class="next__arrow">NEXT</div>
+          <h4 class="next__ttl">SMART MOBILITY<br>MANAGEMENT</h4>
+          <p class="next__sub">社会を前進させる技術</p>
+        </a>
+        <div class="next__bg"></div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section class="home-conts__item" id="sensing">
-      <div class="home-conts__inner sensing">
-        <div class="home-conts__bg">
-          <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video">
-            <source src="<?php echo $img_url; ?>/home/sensing.mp4" type="video/mp4">
-          </video>
-          <div class="home-conts__dot"></div>
-        </div>
-        <header class="home-conts__head">
-          <h2 class="home-conts__ttl">SENSING/AI</h2>
-          <p class="home-conts__sub">データの意味を見出し新たな価値を創る</p>
-        </header>
-        <p class="home-conts__ex">すぐに実現可能なAIによる行動分析/予測</p>
+  <section class="home-conts__item" id="mobility">
+    <div class="home-conts__inner mobility">
+      <div class="home-conts__bg">
+        <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video"><source src="<?php echo $img_url; ?>/home/mobility.mp4" type="video/mp4">
+        </video>
+        <div class="home-conts__dot"></div>
       </div>
-    </section>
+      <header class="home-conts__head">
+        <h2 class="home-conts__ttl">SMART MOBILITY<br>MANAGEMENT</h2>
+        <p class="home-conts__sub">社会を前進させる技術</p>
+      </header>
+      <p class="home-conts__ex">シェアリングエコノミーから物流課題までICTでモビリティの未来を創る</p>
+      <div class="home-conts__next next">
+        <a href="#mobility" class="next__link" data-scroll>
+          <div class="next__arrow">NEXT</div>
+          <h4 class="next__ttl">SENSING/AI</h4>
+          <p class="next__sub">データの意味を見出し新たな価値を創る</p>
+        </a>
+        <div class="next__bg"></div>
+      </div>
+    </div>
+  </section>
 
-  </div>
-  <div class="main__conts">
+  <section class="home-conts__item" id="sensing">
+    <div class="home-conts__inner sensing">
+      <div class="home-conts__bg">
+        <video autoplay loop poster="<?php echo $img_url; ?>/home/xxx.png" class="home-conts__video"><source src="<?php echo $img_url; ?>/home/sensing.mp4" type="video/mp4">
+        </video>
+        <div class="home-conts__dot"></div>
+      </div>
+      <header class="home-conts__head">
+        <h2 class="home-conts__ttl">SENSING/AI</h2>
+        <p class="home-conts__sub">データの意味を見出し新たな価値を創る</p>
+      </header>
+      <p class="home-conts__ex">すぐに実現可能なAIによる行動分析/予測</p>
+    </div>
+  </section>
 
-    <section class="conts__item l-wrap">
-      <ul>
-        <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>
-        <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>      <li>
-          <a href="#" target="_blank" rel="nofollow">
-            <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
-          </a>
-        </li>
-      </ul>
-    </section>
-  </div>
+  <section class="conts__item l-wrap">
+    <ul>
+      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>
+      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>      <li>
+        <a href="#" target="_blank" rel="nofollow">
+          <img src="<?php echo $img_url; ?>/dummy/bronze.png" alt="会社ロゴ" class="u-img__max">
+        </a>
+      </li>
+    </ul>
+  </section>
+</div>
 
-  <?php
-  include($inc_path."/lib/inc/main-foot.php");
-  include($inc_path."/lib/inc/pagetop.php");
-  include($inc_path."/lib/inc/footer.php");
-  include($inc_path."/lib/inc/foot.php");
-  ?>
+<?php
+include($inc_path."/lib/inc/main-foot.php");
+include($inc_path."/lib/inc/pagetop.php");
+include($inc_path."/lib/inc/footer.php");
+include($inc_path."/lib/inc/foot.php");
+?>
